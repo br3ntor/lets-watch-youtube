80549cf9d1c7c52d7366367193e11f0c742deffb
br3ntor <br3ntor@gmail.com>
2021-06-02 23:11:42 -0700

    Youtube api for production.

--- client/src/libs/youtube.js
+++ client/src/libs/youtube.js
@@ -2,7 +2,10 @@ export default async function getVideoData(videos) {
   const resource = "https://www.googleapis.com/youtube/v3/videos";
   const part = "part=snippet";
   const id = videos.toString();
-  const key = "AIzaSyCN_A0OB8dFlLjs5oENpuHT-ZCfKf1wupk";
+  const key =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_YTP
+      : process.env.REACT_APP_YTD;
   const url = `${resource}?${part}&id=${id}&key=${key}`;
   const response = await fetch(url);
   return response.json();
